ca15f829227e688e01d7a171a4cf3a8820a8b578
Maria Shtereva <35622017+mariashtereva@users.noreply.github.com>
2020-03-07 15:44:46 +0200

    Add files via upload

---
+++ Problem3/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace employee
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public void IncreaseSalary(double bonus)
+        {
+            if(this.Age <= 30)
+            {
+                this.Salary += this.Salary * bonus / 200;
+            }
+            else
+            {
+                this.Salary += this.Salary * bonus / 100;
+            }
+        }
+
+        public Employee(){ }
+        public Employee(string first_name, string last_name, int age, double salary)
+        {
+           FirstName = first_name;
+           LastName = last_name;
+           Age = age;
+           Salary = salary;
+        }
+
+        
+
+
+
+    }
+}
---
+++ Problem3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> employees = new List<Employee>();
+
+            int lines = int.Parse(Console.ReadLine());
+            
+            while(lines > 0)
+            {
+
+                var cmdArgs = Console.ReadLine().Split().ToArray();
+                employees.Add
+                    (
+                    new Employee()
+                    {
+                        FirstName = cmdArgs[0],
+                        LastName = cmdArgs[1],
+                        Age = int.Parse(cmdArgs[2]),
+                        Salary = double.Parse(cmdArgs[3])
+                    }
+                    );
+                lines--;
+            }
+
+            double bonus = double.Parse(Console.ReadLine());
+         
+
+            // Print
+            foreach (var employee in employees)
+            {
+                employee.IncreaseSalary(bonus);
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} get {employee.Salary:0.00} leva");
+            }
+
+
+        }
+    }
+}
