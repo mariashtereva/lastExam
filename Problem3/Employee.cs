6c40a263977b0a01715b7e2498427fbdffa76c06
Maria Shtereva <35622017+mariashtereva@users.noreply.github.com>
2020-03-07 15:52:02 +0200

    Add files via upload

---
+++ Problem3/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace employee
+{
+    public class Employee
+    {
+        //field for the first name
+        public string FirstName { get; set; }
+        //field for the last name
+        public string LastName { get; set; }
+        //field for the age
+        public int Age { get; set; }
+        //field for the initial salary
+        public double Salary { get; set; }
+
+        //method for the increase of the salary based on the age of the employee
+        public void IncreaseSalary(double bonus)
+        {
+            //if the employee is less than 30
+            if(this.Age <= 30)
+            {
+                //increase of the salary is half the bonus percentage
+                this.Salary += this.Salary * bonus / 200;
+            }
+            //if the employee is more than 30
+            else
+            {
+                //increase of the salary is the full bonus percentage
+                this.Salary += this.Salary * bonus / 100;
+            }
+        }
+
+        //the empty constructor
+        public Employee(){ }
+        //the cvontructor with parameters
+        public Employee(string first_name, string last_name, int age, double salary)
+        {
+           FirstName = first_name;
+           LastName = last_name;
+           Age = age;
+           Salary = salary;
+        }
+
+        
+
+
+
+    }
+}
--- Problem3/Program.cs
+++ Problem3/Program.cs
@@ -9,15 +9,17 @@ namespace employee
     {
         static void Main(string[] args)
         {
-
+            //creates an empty list for the employees
             List<Employee> employees = new List<Employee>();
 
+            //how many lines
             int lines = int.Parse(Console.ReadLine());
-            
+
             while(lines > 0)
             {
-
+                //commands input
                 var cmdArgs = Console.ReadLine().Split().ToArray();
+                //adds employees to the list
                 employees.Add
                     (
                     new Employee()
@@ -31,13 +33,15 @@ namespace employee
                 lines--;
             }
 
+            //bonus percentage input
             double bonus = double.Parse(Console.ReadLine());
          
-
-            // Print
+            //for every employee
             foreach (var employee in employees)
             {
+                //increasing the salary using the method from class Employee
                 employee.IncreaseSalary(bonus);
+                //output
                 Console.WriteLine($"{employee.FirstName} {employee.LastName} get {employee.Salary:0.00} leva");
             }
 
