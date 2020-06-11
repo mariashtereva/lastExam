ab96c8e0c1aa57603f16114553ca171bd3460954
Maria Shtereva <35622017+mariashtereva@users.noreply.github.com>
2020-06-11 16:31:49 +0300

    Add files via upload

---
+++ Problem 10/ConsoleMVC/ConsoleMVC/Controllers/Instructions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC.Controllers
+{
+    public class Instructions
+    {
+        public long Execute(string command)
+        {
+            string[] codeArgs = command.Split(' ');
+            long result = 0;
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        result = operandOne + 1;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        result = operandOne - 1;
+                        break;
+                }
+                case "ADD":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        int operandTwo = int.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        long operandOne = int.Parse(codeArgs[1]);
+                        long operandTwo = int.Parse(codeArgs[2]);
+                        result = (long)(operandOne * operandTwo);
+                        break;
+
+                    }
+            }
+            return result;
+        }
+    }
+}
---
+++ Problem 10/ConsoleMVC/ConsoleMVC/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleMVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instruction = new Instructions();
+            string code = Console.ReadLine();
+            while (code != "END")
+            {
+                Console.WriteLine(instruction.Execute(code));
+                code = Console.ReadLine();
+            }
+        }
+    }
+}
